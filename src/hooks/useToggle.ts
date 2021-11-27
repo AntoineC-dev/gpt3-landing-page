b919bce63379068e2c2f8c04bfc467b52f774ba8
AntoineC-dev <ac.dev@hotmail.com>
2021-11-27 19:54:25 -0400

    Create useToggle Hook

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,9 @@
+import React, { useCallback, useState } from "react";
+
+const useToggle = (initialValue: boolean = false): [boolean, () => void] => {
+  const [value, setValue] = useState(initialValue);
+  const toggleValue = useCallback(() => setValue((prev) => !prev), []);
+  return [value, toggleValue];
+};
+
+export default useToggle;
