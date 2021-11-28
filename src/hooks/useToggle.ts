3d5fe90463a09dc9a6a5a75b5f3e5ffd7f109fb8
AntoineC-dev <ac.dev@hotmail.com>
2021-11-27 21:48:26 -0400

    Remove unused import

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import { useCallback, useState } from "react";
 
 const useToggle = (initialValue: boolean = false): [boolean, () => void] => {
   const [value, setValue] = useState(initialValue);
