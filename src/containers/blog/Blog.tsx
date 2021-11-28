7e17ae342e915b1915dd47296b16ca8eabdf0480
AntoineC-dev <ac.dev@hotmail.com>
2021-11-27 23:34:21 -0400

    Create Blog Container

--- src/containers/blog/Blog.tsx
+++ src/containers/blog/Blog.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Blog.css";
+import { Article } from "../../components";
+
+const data = {
+  title: "A lot is happening, We are blogging about it.",
+};
 
 const Blog = () => {
   return (
-    <div>
-      <h1>Blog</h1>
+    <div className="gpt3__blog section__pading" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">{data.title}</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container__groupA"></div>
+        <div className="gpt3__blog-container__groupB"></div>
+      </div>
     </div>
   );
 };
