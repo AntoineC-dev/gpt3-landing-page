1879612a38b56423155641d69d6faf241220cb3c
AntoineC-dev <ac.dev@hotmail.com>
2021-11-28 01:15:59 -0400

    Create Blog & Article

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { FC } from "react";
 import "./Article.css";
 
-const Article = () => {
+interface IArticle {
+  imgUrl: string;
+  date: string;
+  title: string;
+  featured?: boolean;
+}
+
+const Article: FC<IArticle> = ({ imgUrl, date, title, featured = false }) => {
+  const featuredClass = featured ? "featured" : "";
   return (
-    <div>
-      <h1>Article</h1>
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3 className={`gpt3__blog-container_article-content_title ${featuredClass}`}>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
     </div>
   );
 };
--- src/containers/blog/Blog.tsx
+++ src/containers/blog/Blog.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+
 import "./Blog.css";
 import { Article } from "../../components";
+import { blog1Png, blog2Png, blog3Png, blog4Png, blog5Png } from "../../assets";
+
+const blogImages = [blog2Png, blog3Png, blog4Png, blog5Png];
 
 const data = {
   title: "A lot is happening, We are blogging about it.",
@@ -8,13 +12,29 @@ const data = {
 
 const Blog = () => {
   return (
-    <div className="gpt3__blog section__pading" id="blog">
+    <div className="gpt3__blog section__padding" id="blog">
       <div className="gpt3__blog-heading">
         <h1 className="gradient__text">{data.title}</h1>
       </div>
       <div className="gpt3__blog-container">
-        <div className="gpt3__blog-container__groupA"></div>
-        <div className="gpt3__blog-container__groupB"></div>
+        <div className="gpt3__blog-container_groupA">
+          <Article
+            imgUrl={blog1Png}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            featured
+          />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          {blogImages.map((item, index) => (
+            <Article
+              key={item + index}
+              imgUrl={item}
+              date="Sep 26, 2021"
+              title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
